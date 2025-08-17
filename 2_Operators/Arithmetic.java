0ca158830a089e5aec8f95225fc6fc2415925ff7
royal0236 <royaldavanam02@gmail.com>
2025-08-17 23:05:43 +0530

    Arithmetic Operators Updated

--- 2_Operators/Arithmetic.java
+++ 2_Operators/Arithmetic.java
@@ -4,38 +4,38 @@ public class Arithmetic {
   public static void main(String[] args) {
     Scanner sc=new Scanner(System.in);
     // Take two numbers as input → print sum, difference, product, quotient, remainder.
-    // System.out.println("Checking of Different Arithmetic Operators");
-    // System.out.print("Enter the first number:");
-    //   double first_number=sc.nextDouble();
-    //  System.out.print("Enter the second number:");
-    //   double second_number=sc.nextDouble();
-    //   System.out.println("Sum of Two Numbers is:" +(first_number + second_number));
-    //   System.out.println("Difference of Two Numbers is:" +(first_number - second_number));
-    //   System.out.println("Product of Two Numbers is:" +(first_number * second_number));
-    //   System.out.println("Quotient of Two Numbers is:" +(first_number / second_number));
-    //   System.out.println("Remainder of Two Numbers is:" +(first_number % second_number));
-    //   // Write a program to calculate the area of a triangle.(b*h)/2.
-    //   System.out.println("calculate the area of a triangle");
-    //   System.out.print("Enter the base value:");
-    //   float base=sc.nextFloat();
-    //   System.out.print("Enter the value of height:");
-    //   float height=sc.nextFloat();
-    //   System.out.println("The Area of a Triangle is:" + (base/height)/2);
-    //   //  Input marks of 5 subjects, calculate total & average
-    //   System.out.println("Marks Calculation of a student");
-    //   System.out.print("Enter the subject1_marks:");
-    //   int subject1_marks=sc.nextInt();
-    //   System.out.print("Enter the subject2_marks:");
-    //   int subject2_marks=sc.nextInt();
-    //   System.out.print("Enter the subject3_marks:");
-    //   int subject3_marks=sc.nextInt();
-    //    System.out.print("Enter the subject4_marks:");
-    //   int subject4_marks=sc.nextInt();
-    //    System.out.print("Enter the subject5_marks:");
-    //   int subject5_marks=sc.nextInt();
-    //   int Total_marks=(subject1_marks + subject2_marks + subject3_marks + subject4_marks + subject5_marks);
-    //   System.out.println("Total Marks:" + Total_marks);
-    //   System.out.println("Average marks:" + Total_marks/5);
+    System.out.println("Checking of Different Arithmetic Operators");
+    System.out.print("Enter the first number:");
+      double first_number=sc.nextDouble();
+     System.out.print("Enter the second number:");
+      double second_number=sc.nextDouble();
+      System.out.println("Sum of Two Numbers is:" +(first_number + second_number));
+      System.out.println("Difference of Two Numbers is:" +(first_number - second_number));
+      System.out.println("Product of Two Numbers is:" +(first_number * second_number));
+      System.out.println("Quotient of Two Numbers is:" +(first_number / second_number));
+      System.out.println("Remainder of Two Numbers is:" +(first_number % second_number));
+      // Write a program to calculate the area of a triangle.(b*h)/2.
+      System.out.println("calculate the area of a triangle");
+      System.out.print("Enter the base value:");
+      float base=sc.nextFloat();
+      System.out.print("Enter the value of height:");
+      float height=sc.nextFloat();
+      System.out.println("The Area of a Triangle is:" + (base/height)/2);
+      //  Input marks of 5 subjects, calculate total & average
+      System.out.println("Marks Calculation of a student");
+      System.out.print("Enter the subject1_marks:");
+      int subject1_marks=sc.nextInt();
+      System.out.print("Enter the subject2_marks:");
+      int subject2_marks=sc.nextInt();
+      System.out.print("Enter the subject3_marks:");
+      int subject3_marks=sc.nextInt();
+       System.out.print("Enter the subject4_marks:");
+      int subject4_marks=sc.nextInt();
+       System.out.print("Enter the subject5_marks:");
+      int subject5_marks=sc.nextInt();
+      int Total_marks=(subject1_marks + subject2_marks + subject3_marks + subject4_marks + subject5_marks);
+      System.out.println("Total Marks:" + Total_marks);
+      System.out.println("Average marks:" + Total_marks/5);
       // Convert Minutes into Hours & Minutes
       System.out.println("Converting Minutes into Hours & Minutes.");
       System.out.print("Enter The Minutes:");
